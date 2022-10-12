c7a190450bd39ba863f8ade264bb7d0e4f44ee81
Dornell <rakuzoxilion@gmail.com>
2022-10-12 20:02:42 +0700

    Add function to input book data

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+const bookForm = document.querySelector('.book-form')
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const reads = document.querySelectorAll('input[name=read-status]');
 const myLibrary = [];
 
 function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.isRead = !!isRead;
+    this.isRead = isRead;
 }
 
 function addBookToLibrary(title,author,pages, isRead) {
@@ -12,3 +17,17 @@ function addBookToLibrary(title,author,pages, isRead) {
     myLibrary.push(myObj);
     console.log(myLibrary);
 }
+
+bookForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let tempTitle = title.value;
+    let tempAuthor = author.value;
+    let tempPages = pages.value
+    let readStatus = '';
+    for (let read of reads) {
+        if (read.checked) {
+            readStatus = read.value;
+        }
+    }
+    addBookToLibrary(tempTitle, tempAuthor, tempPages, readStatus);
+});
