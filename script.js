55ef94657b482d58d986e488c4763f82e8acab1d
Dornell <rakuzoxilion@gmail.com>
2022-10-13 19:09:11 +0700

    Add svg delete icon

--- script.js
+++ script.js
@@ -76,7 +76,17 @@ function showLibrary() {
         const delBtn = document.createElement('button');
         delBtn.setAttribute('class', 'delete');
         delBtn.setAttribute('data-key', `${item}`);
-        delBtn.innerText = 'Delete';
+        //SVG Icon
+        const iconSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        const iconPath = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        iconSvg.setAttribute('viewBox', '0 0 24 24');
+        iconPath.setAttribute(
+            'd',
+            'M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z'
+        );
+        iconSvg.appendChild(iconPath);
+        delBtn.appendChild(iconSvg);
+        // delBtn.innerText = 'Delete';
         delBtn.addEventListener('click', e => {
             myLibrary.splice(e.target.dataset.key,1);
             resetTable();
