ec0841ff31978f4d1d718db8f27c0d63dea45a94
Dornell <rakuzoxilion@gmail.com>
2022-10-13 13:18:58 +0700

    Implement show book function

--- script.js
+++ script.js
@@ -3,7 +3,20 @@ const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 const pages = document.querySelector('#pages');
 const reads = document.querySelectorAll('input[name=read-status]');
-const myLibrary = [];
+const myLibrary = [
+    {
+    title: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+    pages: 279,
+    isRead: 'No'
+    },
+    {
+        title: 'The Silent Patient',
+        author: 'Alex Michaelides',
+        pages: 265,
+        isRead: 'Yes'
+    },
+];
 
 function Book(title, author, pages, isRead) {
     this.title = title;
@@ -18,11 +31,47 @@ function addBookToLibrary(title,author,pages, isRead) {
     console.log(myLibrary);
 }
 
+function showLibrary() {
+    for (let item in myLibrary) {
+        const tableRow = document.createElement('tr');
+        tableRow.setAttribute('id', `book-row${item}`)
+        document.getElementById('book-list').appendChild(tableRow);
+
+        const titleData = document.createElement('td');
+        const titleText = document.createTextNode(myLibrary[item].title);
+        titleData.appendChild(titleText);
+        document.getElementById(`book-row${item}`).appendChild(titleData);
+
+        const authorData = document.createElement('td');
+        const authorText = document.createTextNode(myLibrary[item].author);
+        authorData.appendChild(authorText);
+        document.getElementById(`book-row${item}`).appendChild(authorData);
+
+        const pagesData = document.createElement('td');
+        pagesData.setAttribute('class', 'right-text');
+        const pagesText = document.createTextNode(myLibrary[item].pages);
+        pagesData.appendChild(pagesText);
+        document.getElementById(`book-row${item}`).appendChild(pagesData);
+
+        const isReadData = document.createElement('td');
+        isReadData.setAttribute('class', 'center-text');
+        const isReadText = document.createTextNode(myLibrary[item].isRead);
+        isReadData.appendChild(isReadText);
+        document.getElementById(`book-row${item}`).appendChild(isReadData);
+
+        const deleteData = document.createElement('td');
+        deleteData.setAttribute('class', 'center-text');
+        const deleteText = document.createTextNode('Delete');
+        deleteData.appendChild(deleteText);
+        document.getElementById(`book-row${item}`).appendChild(deleteData);
+    }
+}
+
 bookForm.addEventListener('submit', (e) => {
     e.preventDefault();
     let tempTitle = title.value;
     let tempAuthor = author.value;
-    let tempPages = pages.value
+    let tempPages = Number(pages.value);
     let readStatus = '';
     for (let read of reads) {
         if (read.checked) {
